0fb3f764cbdb6f465bf58446940bcb8454e9b316
WinterThane <demonsaso@gmail.com>
2018-05-27 14:16:08 +0200

    Thread

--- Threading/Program.cs
+++ Threading/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Threading
 {
@@ -6,7 +7,24 @@ namespace Threading
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("[{0}] Main start", Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("[{0}] Core count [{1}]", Thread.CurrentThread.ManagedThreadId, Environment.ProcessorCount);
+
+            var t = new Thread(Hello)
+            {
+                Name = "Hello thread",
+                Priority = ThreadPriority.BelowNormal
+            };
+            t.Start();
+
+            Console.WriteLine("[{0}] Main end", Thread.CurrentThread.ManagedThreadId);
+
+            Console.ReadLine();
+        }
+
+        private static void Hello()
+        {
+            Console.WriteLine("[{0}] Hello thread", Thread.CurrentThread.ManagedThreadId);
         }
     }
 }
